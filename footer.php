ce917dc85fc9fe30f38a4cfcaf6917fb8c4b8859
fauzigalih <fauzigalihas@gmail.com>
2020-07-23 23:28:21 +0700

    layout footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600"><span>Copyright © 2019 Designed by <a href="https://colorlib.com" target="_blank" title="Colorlib">Colorlib</a>. All rights
+                reserved.</span></footer>
+    </div>
+</div>
+<script type="text/javascript" src="assets/js/vendor.js"></script>
+<script type="text/javascript" src="assets/js/bundle.js"></script>
+</body>
+
+</html>
