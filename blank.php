4602070e04db9de0087d706d3b22a2ad0a105265
fauzigalih <fauzigalihas@gmail.com>
2020-07-23 23:26:36 +0700

    add page blank

---
+++ blank.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Blank";
+include 'header.php';
+include 'sidebar.php';
+?>
+<main class="main-content bgc-grey-100">
+    <div id="mainContent">
+        <div class="full-container"></div>
+    </div>
+</main>
+<?php include 'footer.php'; ?>
